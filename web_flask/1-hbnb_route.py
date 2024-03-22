7825bd5375792fd71497c2ed9b08b396b02972a1
mahmoud tarek <mahmoud_ta_rek@outlook.com>
2024-03-22 00:04:14 -0700

    l

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Flask framework
-    """
+"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,7 +8,15 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_world():
+    """return hello hbhb
+    """
     return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    """return HBNB"""
+    return "HBNB"
 
 
 if __name__ == "__main__":
