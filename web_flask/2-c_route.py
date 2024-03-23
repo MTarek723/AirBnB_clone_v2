97125f7b0bae59548326babfea97801d319bc622
mahmoud tarek <mahmoud_ta_rek@outlook.com>
2024-03-23 12:19:41 -0700

    L

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,31 +1,28 @@
-
 #!/usr/bin/python3
-"""Flask framework
-"""
+"""script that starts a Flask web application"""
 from flask import Flask
-
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route("/", strict_slashes=False)
-def hello_world():
-    """return hello hbhb
-    """
-    return "Hello HBNB!"
+@app.route('/')
+def hello_hbnb():
+    """print web"""
+    return 'Hello HBNB!'
 
 
-@app.route("/hbnb", strict_slashes=False)
-def HBNB():
-    """return HBNB"""
-    return "HBNB"
+@app.route('/hbnb')
+def hbnb():
+    """print web"""
+    return 'HBNB'
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def text(text):
-    """return text given"""
-    return "C {}".format(text.replace("_", " "))
+@app.route('/c/<text>')
+def c_is_fun(text):
+    """print C followed by the value of the text variable"""
+    return 'C {}'.format(text.replace('_', ' '))
 
 
-if __name__ == "__main__":
-    app.run()
-
+if __name__ == '__main__':
+    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
